e06273c1d32a7430b6188bce18b4c7316aa353f2
satrias825 <114397801+satrias825@users.noreply.github.com>
2023-09-28 07:14:50 +0800

    Add files via upload

---
+++ FrontEnd/JAVASCRIPT/app.js
@@ -0,0 +1,56 @@
+// alert('PERINGATAN!!!')
+// let age=15
+
+// if(age == 15)
+// console.log('silahkan masuk')
+
+// let angka = Math.random();
+// console.log(angka);
+
+// if(angka >= 0.5) {
+//     console.log('gajian')
+// }
+
+// prompt("isi apa aja lah?")
+
+
+//else if statement makin banyal pilihan
+// const inihari = prompt('Isikan hari').toLowerCase();
+
+// if(inihari === 'senin') {
+//     console.log('Selamat bekerja kawan!');
+// } else if (inihari === 'sabtu') {
+//     console.log('Semoga jangan cepat berlalu!');
+// } else if (inihari === 'kamis') {
+//     console.log('Sabar 2 hari lagi')
+
+// // else adalah pilihan terakhir
+// } else {
+//     console.log('semangattt!!!');
+// }
+
+//operator logic and=(&&)
+// const pasword = prompt("isi apa aja lah?")
+
+// //kondisi harus keduanya benar
+// if(pasword.length >= 6 && pasword.indexOf(" ") === -1) {
+//     console.log("passwordnya sudah benar");
+// } else {
+//     console.log("password salah");
+// }
+
+//alternatif lain "if adalah switch" "else adalah default"
+// const merek_motorku = prompt("motormu merek apa?")
+// switch (merek_motorku) {
+//     case "yamaha":
+//         console.log("motor saya");
+//         break;
+//     case "honda":
+//         console.log("bukan motor saya");
+//         break;
+//     default:
+//         console.log("motor apa itu?"); 
+// }
+
+//data array
+let playlist = ["kamu","Aku","Dia"];    
